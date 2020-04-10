f9b431641e77eeeee3c8c6c7b4044b8fb2a5f647
nolanluhm <nolanluhm@gmail.com>
2020-04-09 20:25:27 -0500

    Completed Assignment #6.

---
+++ src/app/satellite.ts
@@ -0,0 +1,27 @@
+export class Satellite {
+
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+
+    this.name = name;
+    this.orbitType = orbitType;
+    this.type = type;
+    this.operational = operational;
+    this.launchDate = launchDate;
+
+    }
+
+    shouldShowWarning() {
+        let satelliteType = "Space Debris";
+
+        if (this.type.toLowerCase() === satelliteType.toLowerCase()) {
+            return true;
+        }
+        return false;
+    }
+}
